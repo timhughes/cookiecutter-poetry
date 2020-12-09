8fa95650a85f7a548a7317009b2fc59cefea9f17
Tim Hughes <thughes@thegoldfish.org>
2020-12-09 17:14:11 +0000

    Update __init__.py

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/__init__.py
@@ -1,3 +1,3 @@
-__author__ = "{{cookiecutter.module_name}} Developers"
+__author__ = "{{ cookiecutter.full_name }} <{{ cookiecutter.email }}>""=
 __version__ = "0.0.1"
-__copyright__ = "© {{cookiecutter.module_name}} Developers"
+__copyright__ = f"© {__author__}"
