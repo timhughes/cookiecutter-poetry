a4fec59ead9f397838f5ffbed6fb3e2f776f7fbd
Tim Hughes <thughes@thegoldfish.org>
2021-03-03 18:03:53 +0000

    Update cli.py

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/cli.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/cli.py
@@ -8,6 +8,22 @@ import {{cookiecutter.module_name}}
 from {{cookiecutter.module_name}}.app import App
 from {{cookiecutter.module_name}}.logging import logger
 
+if sys.stdout.isatty():
+# You're running in a real terminal
+    LOG_FORMAT="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+else:
+    LOG_FORMAT="%(name)s - %(levelname)s - %(message)s"
+
+logging.basicConfig(
+    level=getattr(logging, os.getenv("LOGLEVEL", "INFO").upper()),
+    format=LOG_FORMAT,
+    datefmt="%Y-%m-%dT%H:%M:%S"
+)
+
+logger = logging.getLogger("{{cookiecutter.module_name}}")
+
+# set levels for other modules
+logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 @click.group()
 def cli():
