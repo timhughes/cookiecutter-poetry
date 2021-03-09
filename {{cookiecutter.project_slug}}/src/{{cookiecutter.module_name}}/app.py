efc01397177666bffeb7597ea4ec856ed755f5c1
Tim Hughes <thughes@thegoldfish.org>
2021-03-09 09:36:06 +0000

    Update app.py

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/app.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/app.py
@@ -3,6 +3,9 @@ import logging
 logger = logging.getLogger("{{cookiecutter.module_name}}")
 
 class App:
+    
+    def __init__(self, settings):
+        self.settings = settings    
 
     def start(self):
         print("Starting")
