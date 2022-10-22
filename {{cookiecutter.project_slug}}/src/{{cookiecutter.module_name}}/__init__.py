1ee0e17e2797e064310f981b8fe60954c55044b8
Tim Hughes <thughes@thegoldfish.org>
2022-10-22 23:36:02 +0100

    Get version info from importlib.metadata if it is needed

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/__init__.py
@@ -1,9 +1,1 @@
-"""
-{{ cookiecutter.short_description }}
-"""
-__author__ = "{{ cookiecutter.full_name }} <{{ cookiecutter.email }}>"
-__version__ = "{{ cookiecutter.version }}"
-__version_info__ = tuple([ int(num) for num in __version__.split('.')])
 
-
-__all__ = []
