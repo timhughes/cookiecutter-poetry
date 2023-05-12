de805765a2328a889afd1ac78e971378be5e85b9
Tim Hughes <thughes@thegoldfish.org>
2023-05-12 22:09:01 +0100

    fix version discovery

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/app.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/app.py
@@ -1,12 +1,14 @@
-
 import logging
+
 logger = logging.getLogger("{{cookiecutter.module_name}}")
 
-class App:
 
-    def start(self):
+class App:
+    def __init__(self, settings) -> None:
+        self.settings = settings
+
+    def start(self) -> None:
         print("Starting")
 
-    def stop(self):
+    def stop(self) -> None:
         print("Stopping")
-
